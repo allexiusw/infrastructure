c7c7276e6b0ab39faf26ff365ed625a766ecedea
allex <william.al1379@gmail.com>
2023-04-01 01:26:47 -0600

    Update two triggers

--- main.tf
+++ main.tf
@@ -5,12 +5,31 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
-resource "google_cloudbuild_trigger" "filename-trigger" {
+// Cloud build tools partially working
+resource "google_cloudbuild_trigger" "filename-trigger-dev" {
   location = "global"
   name = "easysv-cloud-build-dev"
 
   trigger_template {
     branch_name = "^(main|master)$"
+    repo_name   = "easysv"
+    project_id = "easysv-project-dev"
+  }
+
+  substitutions = {
+    _REPO = "easysv"
+  }
+
+  filename = "cloudbuild-dev.gke.yaml"
+}
+
+// Cloud build dev-tag tools partially working
+resource "google_cloudbuild_trigger" "filename-trigger-dev-tag" {
+  location = "global"
+  name = "easysv-cloud-build-dev-tag"
+
+  trigger_template {
+    tag_name =  "^(dev-.*)$"
     repo_name   = "easysv"
     project_id = "easysv-project-dev"
   }
