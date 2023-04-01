563cad86b7024719e3bb3855be19dea6f7d42c05
allex <william.al1379@gmail.com>
2023-04-01 11:41:21 -0600

    Add k8s cluster to gcp

--- main.tf
+++ main.tf
@@ -13,6 +13,16 @@ variable "tf_state_gcs_dev" {
   default = "easysv-tf-state-dev"
 }
 
+variable "cluster_name" {
+  type = string
+  default = "easysv-k8s-0"
+}
+
+variable "location" {
+  type = string
+  default = "global"
+}
+
 provider "google" {
   project = var.project_id
   region  = var.region
@@ -62,4 +72,39 @@ resource "google_cloudbuild_trigger" "filename-trigger-dev-tag" {
   }
 
   filename = "cloudbuild-dev.gke.yaml"
+}
+
+resource "google_service_account" "main" {
+  account_id   = "${var.cluster_name}-sa"
+  display_name = "GKE Cluster ${var.cluster_name} Service Account"
+}
+
+resource "google_container_cluster" "main" {
+  name               = "${var.cluster_name}"
+  location           = var.location
+  initial_node_count = 3
+  node_config {
+    service_account = google_service_account.main.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+}
+
+resource "time_sleep" "wait_30_seconds" {
+  depends_on = [google_container_cluster.main]
+  create_duration = "30s"
+}
+
+module "gke_auth" {
+  depends_on           = [time_sleep.wait_30_seconds]
+  source               = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  project_id           = var.project_id
+  cluster_name         = google_container_cluster.main.name
+  location             = var.location
+  use_private_endpoint = false
 }
